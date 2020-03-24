62f61d612055be2127cc17bdf54231cca6dc6307
SrYoda <59931831+SrYoda@users.noreply.github.com>
2020-03-24 19:07:05 +0000

    Initial commit

---
+++ app/src/main/java/sise/tecnico/mc/project/insureapp/insureClaim.java
@@ -0,0 +1,14 @@
+package sise.tecnico.mc.project.insureapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insureClaim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insure_claim);
+    }
+}
